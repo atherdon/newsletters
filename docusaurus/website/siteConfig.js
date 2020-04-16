d75e350587e5d3d89d3b3b4e31f66dbb998acc7b
Vadim <vadim.putrov@gmail.com>
2020-04-16 20:34:48 +0300

    update

--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -39,14 +39,9 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
-  users,
-
   /* path to images for header/footer */
   headerIcon: 'img/favicon.ico',
   footerIcon: 'img/favicon.ico',
