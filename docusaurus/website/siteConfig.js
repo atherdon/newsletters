c6dbccd561819dcc93d991233143eb47f2cbf97d
Vadim <vadim.putrov@gmail.com>
2020-04-17 20:23:53 +0300

    change config

--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://vadim9999.github.io', // Your website URL
+  baseUrl: '/newsletters/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
